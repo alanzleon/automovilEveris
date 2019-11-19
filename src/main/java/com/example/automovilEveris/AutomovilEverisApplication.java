27e3af70e86e517ed900d67024d47825ac2a0445
calc2019 <loboscigna@gmail.com>
2019-11-19 19:05:05 -0300

    primer commit

---
+++ src/main/java/com/example/automovilEveris/AutomovilEverisApplication.java
@@ -0,0 +1,13 @@
+package com.example.automovilEveris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomovilEverisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomovilEverisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/automovilEveris/AutomovilEverisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.automovilEveris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomovilEverisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
